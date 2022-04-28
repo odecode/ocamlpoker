cccc5a8c2ecdf6e90c9649b9386765b49e0f955d
Otto <westerlundotto@gmail.com>
2022-04-28 14:31:53 +0300

    better betting logic

--- poker/bin/bot.ml
+++ poker/bin/bot.ml
@@ -5,8 +5,7 @@ let calc_bet (base : float) (hand_score : int) (bluffFactor : float) (bluffCompa
   if bluffFactor < bluffComparisonFactor then
 
 
-
-Float.mul (Float.mul (base) (Float.of_int hand_score)) (Float.mul 100.0 (Float.sub bluffComparisonFactor bluffFactor)) else
+Float.mul (Float.add (base) (log(Float.of_int hand_score)))((Float.add 1.0 (Float.sub bluffComparisonFactor bluffFactor))) else
  Float.mul (base) (log (Float.of_int hand_score))
 
 let bot_bet (bot: player) : int =
@@ -17,7 +16,11 @@ let bot_bet (bot: player) : int =
   let bluffCompareTo = Random.float 1.0 in
   
   let bet_sum = calc_bet base_bet score bluffFactor bluffCompareTo in
- 
+  let () = print_endline (string_of_int score) in
+  let () = print_endline (Float.to_string base_bet) in
+  let () = print_endline (Float.to_string bluffFactor) in
+  let () = print_endline (Float.to_string bluffCompareTo) in
+  let () = print_endline (Float.to_string bet_sum) in
   let largerThan = compare bet_sum (Float.of_int bot.cash) in
 
   if largerThan > 0 then bot.cash else 
